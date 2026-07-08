4df8ccc25c76f8d304b12ad0f8043bf00999fb17
高橋遣 <kentakahashi@takahashitsukainoMacBook-Air.local>
2026-07-08 14:21:31 +0900

    add caption field to instagram api

--- functions/api/instagram.js
+++ functions/api/instagram.js
@@ -1,16 +1,8 @@
 export async function onRequest(context) {
   const token = await context.env.SUSAKI_TOKEN_STORE.get('instagram:token');
-
-  if (!token) {
-    return Response.json({ error: 'Token not found' }, { status: 500 });
-  }
-
-  const res = await fetch(
-    `https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink,timestamp&limit=6&access_token=${token}`
-  );
-
+  if (!token) return Response.json({ error: 'Token not found' }, { status: 500 });
+  const res = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,thumbnail_url,permalink,timestamp,caption&limit=6&access_token=${token}`);
   const data = await res.json();
-
   return Response.json(data, {
     headers: {
       'Access-Control-Allow-Origin': '*',
